f7f595c3865ab67ab397d0882c6f42f571cad69f
DanielPCaruso <daniez.paulaz@gmail.com>
2022-07-18 18:49:57 -0300

    inclusão projetos finais

---
+++ Módulo 4 - projeto/Projeto 4/src/components/header/styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 70%;
+  justify-content: space-between;
+  padding: 4px;
+  margin: 100px 200px 0px;
+
+  input {
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    width: 90%;
+    height: 44px;
+    padding: 8px;
+    font-weight: 400;
+  }
+
+  button {
+    background-color: #76c0e8;
+    padding: 8px 16px;
+    margin: 0 16px;
+    border-radius: 8px;
+
+    &:hover {
+      background-color: #76e8a6;
+      box-shadow: 3px 2px 10px rgba(0, 0, 0, 0.2);
+    }
+
+    span {
+      font-weight: bold;
+      color: #ffff;
+    }
+  }
+`;
---
+++ Módulo 4 - projeto/Projeto 4/src/components/no-search/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./styled";
+
+const NoSearch = () => (
+ <S.Wrapper>
+      <h1>Digite o nome do User do GitHub que você deseja pesquisar!</h1>
+    </S.Wrapper>
+);
+
+export default NoSearch;
